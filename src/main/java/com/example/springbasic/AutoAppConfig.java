27ec0a9f8b95d49d20be2e93dfd8a76d4a85def7
Kim yonggwan <sssw0101@naver.com>
2022-03-04 01:01:38 +0900

    중복 등록과 충돌

--- src/main/java/com/example/springbasic/AutoAppConfig.java
+++ src/main/java/com/example/springbasic/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.example.springbasic;
 
+import com.example.springbasic.member.MemberRepository;
+import com.example.springbasic.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,10 @@ import org.springframework.context.annotation.FilterType;
         (
                 excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+
+    @Bean(name="memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
