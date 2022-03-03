3eefb1e6355e63fbcf9edc41508c58331a4c5dff
Kim yonggwan <sssw0101@naver.com>
2022-03-03 23:54:08 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/com/example/springbasic/AutoAppConfig.java
+++ src/main/java/com/example/springbasic/AutoAppConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan  // 스프링 빈을 자동으로 만들어주는 어노테이션
-        (excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+        (
+                excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 
 }
