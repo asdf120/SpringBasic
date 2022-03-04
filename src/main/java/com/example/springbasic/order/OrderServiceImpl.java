d33d1800c4d6110937bff5f161e403f7419205df
Kim yonggwan <sssw0101@naver.com>
2022-03-04 23:01:16 +0900

    롬복과 최신 트렌드

--- src/main/java/com/example/springbasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbasic/order/OrderServiceImpl.java
@@ -3,21 +3,22 @@ package com.example.springbasic.order;
 import com.example.springbasic.discount.DiscountPolicy;
 import com.example.springbasic.member.Member;
 import com.example.springbasic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository;
 
+    private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     // 생성자를 통한 의존주입
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     // setter를 통한 의존 주입
