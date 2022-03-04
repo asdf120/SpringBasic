cd44ffa02be3ac43c7b32e6ce8d976ccbe2bafa4
Kim yonggwan <sssw0101@naver.com>
2022-03-04 23:59:37 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springbasic/discount/RateDiscountPolicy.java
@@ -3,9 +3,11 @@ package com.example.springbasic.discount;
 
 import com.example.springbasic.member.Grade;
 import com.example.springbasic.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
