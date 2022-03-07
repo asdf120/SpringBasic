bbf05551e7abe00a7b3f9c57c517f697f4af5d68
Kim yonggwan <sssw0101@naver.com>
2022-03-07 16:38:58 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결
    JSR-330 Provider 적용

--- src/test/java/com/example/springbasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/example/springbasic/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,10 +50,10 @@ public class SingletonWithPrototypeTest1 {
 //        }
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
