4b19b4b454801ff3b8b100ab2d0dcdef0a361c4c
Kim yonggwan <sssw0101@naver.com>
2022-03-07 13:04:33 +0900

    빈 등록 초기화, 소멸 메소드

--- src/test/java/com/example/springbasic/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springbasic/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package com.example.springbasic.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
     
     private String url;
 
@@ -31,16 +28,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존 관계주입이 끝나면 호출되는 메소드
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
     //빈이 종료되면 호출
-    @Override
-    public void destroy() throws Exception {
+    public void close(){
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
